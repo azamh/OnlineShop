23a8eeb16f7448b7cc9a182c0974976311f7b087
Azam Hosseini <azam.hosseini@gesis.org>
2020-01-27 09:54:55 +0100

    uploaf files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBuMr42ySirH29PYA21G2PPIBvXmu117lo",
+    authDomain: "onlineshop-21d2b.firebaseapp.com",
+    databaseURL: "https://onlineshop-21d2b.firebaseio.com",
+    projectId: "onlineshop-21d2b",
+    storageBucket: "onlineshop-21d2b.appspot.com",
+    messagingSenderId: "369465177045",
+    appId: "1:369465177045:web:74677740a4340f4331c35c",
+    measurementId: "G-CP2E3HF616"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBuMr42ySirH29PYA21G2PPIBvXmu117lo",
+    authDomain: "onlineshop-21d2b.firebaseapp.com",
+    databaseURL: "https://onlineshop-21d2b.firebaseio.com",
+    projectId: "onlineshop-21d2b",
+    storageBucket: "onlineshop-21d2b.appspot.com",
+    messagingSenderId: "369465177045",
+    appId: "1:369465177045:web:74677740a4340f4331c35c",
+    measurementId: "G-CP2E3HF616"
+  }
+};
